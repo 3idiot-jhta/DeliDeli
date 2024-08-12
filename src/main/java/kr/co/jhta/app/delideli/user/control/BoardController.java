a6bdeac42b0d56bb1be541ab65c199ce68d50e06
shinsil0601 <133615162+shinsil0601@users.noreply.github.com>
2024-08-13 02:42:49 +0900

    마이페이지-내문의 등록/상세/삭제 (#53)
    * 포인트 충전
    
    * 포인트 충전 - 수정완료
    
    * 포인트 충전 (#41) 수정완료
    
    * 사용자/게시판(공지사항, 이벤트)
    
    * test1
    
    * userCategory Test1
    
    * Test (#7)
    
    * Feat/category (#9)
    
    * feat/Spring Security index 세팅
    
    * test commit
    
    * test/수정
    
    * Test
    
    * test4
    
    ---------
    
    Co-authored-by: DESKTOP-MST6EOC\ksna0303 <ksna0303@gmail.com>
    Co-authored-by: JunHyeng <95612064+JunHyeng@users.noreply.github.com>
    
    * 포인트 충전 (#40)
    
    * first setting commit
    
    * view setting v1.0
    
    * fix/admin-mainpage
    
    * fix/admin-mainpage
    
    * fix/admin-mainpage
    
    * fix/admin-mainpage
    
    * fix/admin-mainpage
    
    * feat/로그인, 회원가입, 뷰세팅
    
    * feat/gitignore 세팅
    
    * Update .gitignore to exclude bin directory
    
    * USER - 내 정보 수정
    
    * first-setting
    
    * 이종우 setting
    
    * UserMapper Setting
    
    * USER - 내 주소 관리
    
    * feat/오류수정
    
    * USER - 게시판 추가, 주석 추가
    
    * feat/Archive-사장님 로그인
    
    * feat/Archive - 사장님 로그인
    
    * USER- 카테고리, 가게목록
    
    * feat/Archive - 사장님 회원가입
    
    * feat/Archive 사장님 accout 완료
    
    * 포인트 충전
    
    * 포인트 충전 - 수정완료
    
    ---------
    
    Co-authored-by: DESKTOP-CBAV1V6\ksna0 <ksna0303@gmail.com>
    Co-authored-by: JunHyeng <95612064+JunHyeng@users.noreply.github.com>
    Co-authored-by: JW <sun991014@gmail.com>
    Co-authored-by: Holic99s <106722767+Holic99s@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: DESKTOP-MST6EOC\ksna0303 <ksna0303@gmail.com>
    Co-authored-by: JunHyeng <95612064+JunHyeng@users.noreply.github.com>
    Co-authored-by: JW <sun991014@gmail.com>
    Co-authored-by: Holic99s <106722767+Holic99s@users.noreply.github.com>
    
    * admin account
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/board/controller directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/board/dto directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/board/controller directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/board/dto directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/board/repository directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/board/repository directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/category directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/main directory
    
    * setting delete
    
    * Delete src/main/java/kr/co/jhta/app/delideli/util directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/config directory
    
    * Delete src/main/resources/mybatis/mapper/user/main directory
    
    * Delete src/main/resources/mybatis/mapper/user/category directory
    
    * Delete src/main/resources/mybatis/mapper/user/board directory
    
    * Delete src/main/java/kr/co/jhta/app/delideli/user/category directory
    
    * setting delete category
    
    * feat/ Achive 관리자 로그인, 사용자 헤더 프로필
    
    * USER - 카테고리, 가게정보, 장바구니, 찜
    
    * fix - 이미지 저장경로
    
    * fear/Archive 사용자 로그인, 회원가입
    
    * Feat/Archive 사용자 로그인, 회원가입
    
    * 마이페이지- 내문의 조회/등록/삭제
    
    ---------
    
    Co-authored-by: DESKTOP-MST6EOC\ksna0303 <ksna0303@gmail.com>
    Co-authored-by: JunHyeng <95612064+JunHyeng@users.noreply.github.com>
    Co-authored-by: JW <sun991014@gmail.com>
    Co-authored-by: Holic99s <106722767+Holic99s@users.noreply.github.com>

--- src/main/java/kr/co/jhta/app/delideli/user/control/BoardController.java
+++ src/main/java/kr/co/jhta/app/delideli/user/control/BoardController.java
@@ -6,6 +6,7 @@ import kr.co.jhta.app.delideli.user.account.service.UserService;
 import kr.co.jhta.app.delideli.user.board.domain.Board;
 import kr.co.jhta.app.delideli.user.board.domain.Comment;
 import kr.co.jhta.app.delideli.user.board.service.BoardService;
+import kr.co.jhta.app.delideli.user.board.service.BoardServiceImpl;
 import kr.co.jhta.app.delideli.user.board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,12 +32,18 @@ public class BoardController {
     private final CommentService commentService;
     @Autowired
     private final UserService userService;
+    @Autowired
+    private BoardServiceImpl boardServiceImpl;
 
     // 공지사항 목록
     @GetMapping("/notice")
     public String notice(Model model,
                          @RequestParam(name = "keyword", defaultValue = "none") String keyword,
-                         @RequestParam(name = "page", defaultValue = "1") int page) {
+                         @RequestParam(name = "page", defaultValue = "1") int page, @AuthenticationPrincipal User user) {
+        if (user != null) {
+            UserAccount userAccount = userService.findUserById(user.getUsername());
+            model.addAttribute("user", userAccount);
+        }
         int totalNumber = 0;
         int recordPerPage = 5;
         Map<String, Object> map;
@@ -65,7 +72,11 @@ public class BoardController {
 
     // 공지사항 상세보기
     @GetMapping("/detail/{num}")
-    public String detail(@PathVariable int num, Model model) {
+    public String detail(@PathVariable int num, @AuthenticationPrincipal User user, Model model) {
+        if (user != null) {
+            UserAccount userAccount = userService.findUserById(user.getUsername());
+            model.addAttribute("user", userAccount);
+        }
         Board board = boardService.readOneNotice(num);
         model.addAttribute("board", board);
         return "/user/board/noticeDetail";
@@ -74,7 +85,11 @@ public class BoardController {
     // 이벤트 목록
     @GetMapping("/event")
     public String event(Model model, @RequestParam(name = "keyword", defaultValue = "none") String keyword,
-                        @RequestParam(name = "page", defaultValue = "1") int page) {
+                        @RequestParam(name = "page", defaultValue = "1") int page,@AuthenticationPrincipal User user) {
+        if (user != null) {
+            UserAccount userAccount = userService.findUserById(user.getUsername());
+            model.addAttribute("user", userAccount);
+        }
         int totalNumber = 0;
         int recordPerPage = 5;
         Map<String, Object> map;
@@ -113,7 +128,11 @@ public class BoardController {
 
     // 댓글 삽입
     @PostMapping("/eventDetail/comment/write")
-    public String insertComment(@ModelAttribute Comment comment) {
+    public String insertComment(@ModelAttribute Comment comment, @AuthenticationPrincipal User user, Model model) {
+        if (user != null) {
+            UserAccount userAccount = userService.findUserById(user.getUsername());
+            model.addAttribute("user", userAccount);
+        }
         commentService.getCommentsByBoardKey(comment);
         return "redirect:/user/eventDetail/" + comment.getBoardKey();
     }
@@ -121,7 +140,11 @@ public class BoardController {
     // 댓글 조회 ajax 처리
     @GetMapping("/getCommentList")
     @ResponseBody
-    public List<Comment> getCommentList(@RequestParam("boardKey") int boardKey) {
+    public List<Comment> getCommentList(@RequestParam("boardKey") int boardKey, @AuthenticationPrincipal User user, Model model) {
+        if (user != null) {
+            UserAccount userAccount = userService.findUserById(user.getUsername());
+            model.addAttribute("user", userAccount);
+        }
         return commentService.getCommentAll(boardKey);
     }
 
@@ -160,4 +183,54 @@ public class BoardController {
         commentService.insertReplyComment(comment);
         return "success";
     }
+
+    //내문의 목록창이동
+    @GetMapping("/myAsk")
+    public String myAsk(@AuthenticationPrincipal User user, Model model) {
+        UserAccount userAccount = userService.findUserById(user.getUsername());
+        model.addAttribute("user", userAccount);
+        log.info("userKey>>>>>>>>>!!! " + userAccount.getUserKey());
+        List<Board> list;
+
+        list = boardService.getMyAskList(userAccount.getUserKey());
+        model.addAttribute("list", list);
+
+        return "/user/mypage/myAsk";
+    }
+
+    //내문의 글작성 창이동
+    @GetMapping("/myAskWrite")
+    public String myAskWrite(@AuthenticationPrincipal User user, Model model) {
+        UserAccount userAccount = userService.findUserById(user.getUsername());
+        model.addAttribute("user", userAccount);
+        return "/user/mypage/myAskWrite";
+    }
+
+    //내문의 글작성
+    @PostMapping("/myAskWrite")
+    public String myAskWrite(@AuthenticationPrincipal User user, @ModelAttribute Board board) {
+        UserAccount userAccount = userService.findUserById(user.getUsername());
+        board.setUserKey(userAccount.getUserKey().intValue());  // Long 타입을 int 타입으로 변환
+        boardService.myAskWrite(board);  // Board 객체를 매개변수로 전달
+
+        return "redirect:/user/myAsk";
+    }
+
+    //내문의 상세보기
+    @GetMapping("/myAskDetail/{boardKey}")
+    public String myAskDetail(@AuthenticationPrincipal User user, @PathVariable int boardKey, Model model) {
+        UserAccount userAccount = userService.findUserById(user.getUsername());
+        Board board = boardService.myAskDetail(boardKey);
+        model.addAttribute("user", userAccount);
+        model.addAttribute("board", board);
+        return "/user/mypage/myAskDetail";
+    }
+
+    // 내문의 글 삭제
+    @GetMapping("/myAskDelete/{boardKey}")
+    public String deleteMyAsk(@PathVariable int boardKey) {
+        boardService.myAskDelete(boardKey);
+        return "redirect:/user/myAsk";
+    }
+
 }
