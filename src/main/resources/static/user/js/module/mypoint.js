cef6dbc11c281982b37b87392bd74e38e815239c
Cindy <tlstlf36@naver.com>
2024-08-12 09:30:46 +0900

    포인트 충전 - 수정완료

--- src/main/resources/static/user/js/module/mypoint.js
+++ src/main/resources/static/user/js/module/mypoint.js
@@ -1,10 +1,3 @@
-<script src="https://cdn.iamport.kr/v1/iamport.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"/>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-        crossOrigin="anonymous"></script>
-
 $(function () {
     IMP.init("imp66848612");
 });
